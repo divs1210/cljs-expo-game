f27e1fcf71a230fc230107f2d8225753aa31f1cb
Divyansh Prakash <divyansh.prakash@yahoo.com>
2020-03-30 20:20:36 +0530

    Add react-native-disable dependency

--- src/cljs_expo_game/components.cljs
+++ src/cljs_expo_game/components.cljs
@@ -2,6 +2,7 @@
   (:require [reagent.core :as r]))
 
 (def ReactNative (js/require "react-native"))
+(def RNDisplay (js/require "react-native-display"))
 
 (def status-bar (r/adapt-react-class (.-StatusBar ReactNative)))
 (def text (r/adapt-react-class (.-Text ReactNative)))
@@ -9,6 +10,7 @@
 (def image (r/adapt-react-class (.-Image ReactNative)))
 (def image-background (r/adapt-react-class (.-ImageBackground ReactNative)))
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
+(def display (r/adapt-react-class (.-default RNDisplay)))
 
 (def expo (js/require "expo"))
 (def AtExpo (js/require "@expo/vector-icons"))
